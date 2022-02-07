d2e1513b707069129a9d580783a2d3252c1e241f
Ivan Mikhailov <ivanmihval@yandex.ru>
2022-02-08 00:42:37 +0300

    Add cli.create

--- pyadps/cli.py
+++ pyadps/cli.py
@@ -1,6 +1,13 @@
-import click
 import os
 import os.path
+from datetime import datetime
+from typing import List
+
+import click
+
+from pyadps.helpers import calculate_hashsum
+from pyadps.mail import CoordsData, FileAttachment, Mail, MailAttachmentInfo
+from pyadps.storage import Storage
 
 
 @click.group()
@@ -9,10 +16,87 @@ def cli():
 
 
 @cli.command()
-@click.argument('repo_folder', type=click.Path(exists=True, file_okay=False))
+@click.argument('repo_folder', type=click.Path(exists=True, file_okay=False), default='.')
 def init(repo_folder):
-    os.mkdir(os.path.join(repo_folder, 'adps_messages'))
-    os.mkdir(os.path.join(repo_folder, 'adps_attachments'))
+    os.mkdir(os.path.join(repo_folder, Storage.MESSAGES_FOLDER))
+    os.mkdir(os.path.join(repo_folder, Storage.ATTACHMENTS_FOLDERS))
+
+
+def is_valid_repo_folder(repo_folder: str) -> bool:
+    try:
+        directories = os.listdir(repo_folder)
+    except (NotADirectoryError, FileNotFoundError):
+        return False
+
+    return Storage.MESSAGES_FOLDER in directories and Storage.ATTACHMENTS_FOLDERS in directories
+
+
+@cli.command()
+@click.argument('repo_folder', type=click.Path(exists=True, file_okay=False), default='.')
+def create(repo_folder):
+    click.echo('This is the interactive command for creating mail.')
+
+    if not is_valid_repo_folder(repo_folder):
+        click.echo(f'The folder {repo_folder!r} is not valid repository. Use command init for creating the repository')
+        raise click.Abort()
+
+    coordinates = []
+    add_more_coordinates: bool = True
+    while add_more_coordinates:
+        latitude = click.prompt('Enter the latitude', type=click.FloatRange(min=-90.0, max=90.0))
+        longitude = click.prompt('Enter the latitude', type=click.FloatRange(min=-180.0, max=180.0))
+        coordinates.append(CoordsData(latitude, longitude))
+        add_more_coordinates = click.confirm('Add more recipient coordinates?')
+
+    name = click.prompt('Enter the identity value (name, email, etc)', type=str)
+
+    additional_notes = click.prompt('Additional notes (usually this field is used less than the identity)', type=str)
+    additional_notes = additional_notes or None
+
+    inline_message = click.prompt('Inline message (use attachments for big files)', type=str)
+    inline_message = inline_message or None
+
+    size_by_path = {}
+    add_attachment = click.confirm('Add attachment?')
+    while add_attachment:
+        attachment_path = click.prompt('Enter the path', type=click.File())
+        size_by_path[attachment_path.name] = os.stat(attachment_path.name).st_size
+        add_attachment = click.confirm('Add more attachments?')
+
+    file_attachments: List[FileAttachment] = []
+    mail_attachment_infos: List[MailAttachmentInfo] = []
+    hashsum_by_path = {}
+    with click.progressbar(length=sum(size_by_path.values())) as bar:
+        for attachment_path, size_bytes in size_by_path.items():
+            click.echo(f'Calculating hashsum for {attachment_path!r}...')
+
+            with open(attachment_path, 'rb') as file_stream:
+                hashsum = calculate_hashsum(file_stream)  # type: ignore
+
+            bar.update(size_bytes)
+            hashsum_by_path[attachment_path] = hashsum
+
+            file_attachments.append(FileAttachment(
+                filename=os.path.basename(attachment_path),
+                size_bytes=size_bytes,
+                hashsum_hex=hashsum.hex_digest,
+            ))
+            mail_attachment_infos.append(MailAttachmentInfo(
+                path=attachment_path,
+                hashsum_hex=hashsum.hex_digest,
+            ))
+
+    message = Mail(
+        date_created=datetime.now(),
+        recipient_coords=coordinates,
+        name=name,
+        additional_notes=additional_notes,
+        inline_message=inline_message,
+        attachments=file_attachments
+    )
+
+    storage = Storage(repo_folder)
+    storage.save_mail(mail=message, mail_attachment_infos=mail_attachment_infos, target_folder_path=repo_folder)
 
 
 if __name__ == '__main__':
--- pyadps/tests/test_cli.py
+++ pyadps/tests/test_cli.py
@@ -1,6 +1,11 @@
-from click.testing import CliRunner
-from pyadps.cli import init
+import json
+import os
 from os import listdir
+
+from click.testing import CliRunner
+from freezegun import freeze_time
+
+from pyadps.cli import init, create
 
 
 class TestRepoInit:
@@ -13,3 +18,71 @@ class TestRepoInit:
         result = CliRunner().invoke(init, [str(tmp_path)+'not_exists'])  # type: ignore
         assert result.exit_code == 2
         assert set(listdir(tmp_path)) == set()
+
+
+class TestMailCreate:
+    @freeze_time("2018-03-17T12:06:54")
+    def test_ok(self, tmp_path):
+        os.mkdir(tmp_path / 'adps_messages')
+        os.mkdir(tmp_path / 'adps_attachments')
+
+        test_file_content_1 = b''
+        test_file_content_2 = b'456123'
+
+        with open(tmp_path / 'empty_file.12', 'wb') as stream_1:
+            stream_1.write(test_file_content_1)
+
+        with open(tmp_path / 'test_file.txt', 'wb') as stream_2:
+            stream_2.write(test_file_content_2)
+
+        input_rows = [
+            '12.345',  # First lat
+            '44.890',  # First lon
+            'y',  # Add more coordinates
+            '-66.11',  # Second lat
+            '178.43',  # Second lon
+            'n',  # Stop adding more coordinates
+            'name@mail.domain',  # Identity name
+            'Bob Adam',  # Additional notes
+            'Return my book please!',  # Inline message
+            'y',  # Add attachment
+            str(tmp_path / 'empty_file.12'),  # Path to the first attachment,
+            'y',  # Add more attachments
+            str(tmp_path / 'test_file.txt'),  # Path to the first attachment,
+            'n',  # Do not add more attachments
+        ]
+
+        result = CliRunner().invoke(create, [str(tmp_path)], input='\n'.join(input_rows))  # type: ignore
+        assert result.exit_code == 0
+
+        assert os.listdir(tmp_path / 'adps_messages') == ['ee17177185.json']
+        assert os.listdir(tmp_path / 'adps_attachments') == ['ca3d1dde02.bin', 'cf83e1357e.bin']
+
+        with open(tmp_path / 'adps_messages' / 'ee17177185.json') as msg_stream:
+            msg = json.load(msg_stream)
+
+        assert msg == {
+            'additional_notes': 'Bob Adam',
+            'attachments': [
+                {
+                    'filename': 'empty_file.12',
+                    'hashsum_alg': 'sha512',
+                    'hashsum_hex': 'cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce'
+                                   '47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e',
+                    'size_bytes': 0
+                },
+                {
+                    'filename': 'test_file.txt',
+                    'hashsum_alg': 'sha512',
+                    'hashsum_hex': 'ca3d1dde02c4b15d2e95521e259c5e08aaea8feaa722ba14014605249efe3f24'
+                                   '8db3d98aa7c4accbe887e1b40573d7eba71017c5df029c16c8d6f06b0ffda310',
+                    'size_bytes': 6
+                }
+            ],
+            'date_created': '2018-03-17T12:06:54',
+            'inline_message': 'Return my book please!',
+            'min_version': '1.0',
+            'name': 'name@mail.domain',
+            'recipient_coords': [{'lat': 12.345, 'lon': 44.89}, {'lat': -66.11, 'lon': 178.43}],
+            'version': '1.0'
+        }
