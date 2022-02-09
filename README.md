861a58fdb5b80653dd216395bedf4a5f86e37ce1
Ivan Mikhailov <ivanmihval@yandex.ru>
2022-02-09 23:34:53 +0300

    Update worldcities & add license for this file

---
+++ README.md
@@ -0,0 +1,5 @@
+# PyADPS - python lib and CLI for Amateur Digital Post Service
+
+## 3rd party files:
+
+worldcities.csv - downloaded from https://simplemaps.com/data/world-cities , Creative Commons Attribution 4.0
--- pyadps/tests/test_geo_worker.py
+++ pyadps/tests/test_geo_worker.py
@@ -4,13 +4,13 @@ import pytest
 
 from pyadps.geo_worker import City, CoordsTuple, search_city_coords_by_name, search_nearest_city_by_coords
 
-WORLDCITIES_CSV_PATH = str(PurePath(__file__).parents[1] / 'static_files/worldcities.csv')
+WORLDCITIES_CSV_PATH = str(PurePath(__file__).parents[1] / 'static_files/worldcities/worldcities.csv')
 
 
 class TestSearchCityCoordsByName:
     @pytest.mark.parametrize('city_name, lat, lon', [
         ['osAka', 34.75, 135.4601],
-        ['varna', 43.2156, 27.8953],
+        ['varna', 43.2114, 27.9111],
         ['Unknown City', None, None]
     ])
     def test_ok(self, city_name: str, lat: float, lon: float):
@@ -22,9 +22,9 @@ class TestSearchCityCoordsByName:
 
 class TestSearchNearestCityByCoords:
     @pytest.mark.parametrize('lat, lon, city_name, city_name_ascii', [
-        [50.418131, 28.9309657, 'Zhytomyr', 'Zhytomyr'],
+        [50.2745, 28.7005, 'Zhytomyr', 'Zhytomyr'],
         [53.3595118, -6.3086148, 'Dublin', 'Dublin'],
-        [-23.5311317, -46.9026668, 'São Paulo', 'Sao Paulo']
+        [-23.5501, -46.6203, 'São Paulo', 'Sao Paulo']
     ])
     def test_ok(self, lat: float, lon: float, city_name: str, city_name_ascii: str):
         city = search_nearest_city_by_coords(CoordsTuple(lat, lon), WORLDCITIES_CSV_PATH)
